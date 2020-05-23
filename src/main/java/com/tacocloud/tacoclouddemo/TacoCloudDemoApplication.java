2ac83d2cdfffadffeb6f489ceeef978aad22838d
DavidLiu642874264 <642874264@qq.com>
2020-05-23 23:28:26 +0800

    new file:   src/main/java/com/tacocloud/tacoclouddemo/TacoCloudDemoApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/tacocloud/tacoclouddemo/TacoCloudDemoApplicationTests.java 	.gitignore 	.mvn/ 	mvnw 	mvnw.cmd 	pom.xml

---
+++ src/main/java/com/tacocloud/tacoclouddemo/TacoCloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tacocloud.tacoclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tacocloud/tacoclouddemo/TacoCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacocloud.tacoclouddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
